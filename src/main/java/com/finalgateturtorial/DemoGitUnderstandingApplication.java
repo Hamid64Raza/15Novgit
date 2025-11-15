2e87eca4ba04f00358d4da61b971b7f45b167ee7
Hamid64Raza <hamidraza.raza@gamil.com>
2025-11-15 15:37:58 +0530

    first commit

---
+++ src/main/java/com/finalgateturtorial/DemoGitUnderstandingApplication.java
@@ -0,0 +1,13 @@
+package com.finalgateturtorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitUnderstandingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitUnderstandingApplication.class, args);
+	}
+
+}
